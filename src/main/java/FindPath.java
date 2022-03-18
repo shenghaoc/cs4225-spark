0a6994597b5066572099161f7e219978d46a1cbc
shenghaoc <shenghaoc@outlook.com>
2022-03-19 02:36:41 +0800

    Add distance calculation

--- src/main/java/FindPath.java
+++ src/main/java/FindPath.java
@@ -79,9 +79,29 @@ public class FindPath {
                 .select("src", "dst");
 
         Dataset<Row> deadEnds = e.select("dst").except(e.select("src"));
-        e = e.unionByName(deadEnds.select("dst").withColumnRenamed("dst", "src").withColumn("dst", functions.lit(null).cast("long")));
+        e = e.unionByName(deadEnds.select("dst").withColumnRenamed("dst", "src").withColumn("dst",
+                functions.lit(null).cast("Long")));
+
+        Dataset<Row> v1 = v.withColumnRenamed("id", "src").withColumnRenamed("_lat", "lat1").withColumnRenamed("_lon",
+                "lon1");
+        Dataset<Row> v2 = v.withColumnRenamed("id", "dst").withColumnRenamed("_lat", "lat2").withColumnRenamed("_lon",
+                "lon2");
+
+        org.apache.spark.sql.expressions.UserDefinedFunction udfDistance = functions.udf(
+                (Double lat1, Double lat2, Double lon1, Double lon2) -> distance(lat1, lat2, lon1, lon2),
+                org.apache.spark.sql.types.DataTypes.DoubleType);
+        spark.udf().register("udfDistance", udfDistance);
+        e = e.join(v1, "src")
+                .join(v2, "dst");
+
+        e = e.withColumn("dist",
+                functions.callUDF("udfDistance", e.col("lat1"), e.col("lat2"), e.col("lon1"), e.col("lon2")))
+                .select("src", "dst", "dist");
 
         GraphFrame g = new GraphFrame(v, e);
+
+        v.cache();
+        e.cache();
 
         Dataset<Row> tmpEdges = g.edges()
                 .distinct()
