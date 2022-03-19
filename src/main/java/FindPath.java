ab8b4cea6cd41cb102708b6caccd67ba0f8886cf
shenghaoc <shenghaoc@outlook.com>
2022-03-20 00:55:21 +0800

    Cast input to Long type

--- src/main/java/FindPath.java
+++ src/main/java/FindPath.java
@@ -49,8 +49,8 @@ public class FindPath {
                 .load(args[0]);
         
         Dataset<Row> input = spark.read().text(args[1]);
-        input = input.withColumn("src", functions.split(input.col("value"), " ").getItem(0))
-                .withColumn("dst", functions.split(input.col("value"), " ").getItem(1))
+        input = input.withColumn("src", functions.split(input.col("value"), " ").getItem(0).cast(org.apache.spark.sql.types.DataTypes.LongType))
+                .withColumn("dst", functions.split(input.col("value"), " ").getItem(1).cast(org.apache.spark.sql.types.DataTypes.LongType))
                 .drop("value");
 
         Dataset<Row> highwayDf = wayDf.where("array_contains(tag._k,'highway')");
